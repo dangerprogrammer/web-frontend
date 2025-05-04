ac09b197dc87a3f3ce5e47e01d93ffd62a2ba87f
Patrick <104269678+dangerprogrammer@users.noreply.github.com>
2025-05-03 23:53:39 -0300

    Starting register CSS

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,3 +1,10 @@
+import './page.scss';
+
 export default function RegisterPage() {
-    return <div>Register</div>
+    return <main className='register-container'>
+        <section className='container-header'>
+            <h1 className='header-title'>E-ECO</h1>
+            <h4 className='desc'>Reduze, reutilize, doe!</h4>
+        </section>
+    </main>
 }
